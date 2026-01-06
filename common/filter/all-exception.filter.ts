5c23cb51fd7c90d0c0accd08188b6927aeca898a
Ted <tedlee1444@gmail.com>
2026-01-06 10:44:37 +0900

    fixtimestamp

--- common/filter/all-exception.filter.ts
+++ common/filter/all-exception.filter.ts
@@ -29,7 +29,16 @@ export class AllExceptionFilter implements ExceptionFilter {
       message,
       ...(errors ? { errors } : {}),
       path: req.url,
-      timestamp: new Date().toISOString(),
+      timestamp: new Date().toLocaleString('ko-KR', {
+        timeZone: 'Asia/Seoul',
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false, // 24시간 형식
+      }),
     });
   }
 }
--- common/interceptor/response-format.interceptor.ts
+++ common/interceptor/response-format.interceptor.ts
@@ -17,7 +17,16 @@ export class ResponseFormatInterceptor implements NestInterceptor {
         data,
         meta: {
           path: context.switchToHttp().getRequest().url,
-          timestamp: new Date().toLocaleTimeString(),
+          timestamp: new Date().toLocaleString('ko-KR', {
+            timeZone: 'Asia/Seoul',
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false, // 24시간 형식
+          }),
           responseTime: `${Date.now() - now}ms`,
         },
       })),
