6a3bd1cd7fd1db45f4b8677de620cab0600ae7b1
Ted <tedlee1444@gmail.com>
2026-01-07 16:07:05 +0900

    villager1

--- src/villagers/villagers.controller.ts
+++ src/villagers/villagers.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, Get, Post, Param } from '@nestjs/common';
+import { Controller, Get, Post, Param, ParseIntPipe } from '@nestjs/common';
 import { VillagersService } from './villagers.service';
+import { VillagerPreviewDTO } from './dto/villager-preview.response.dto';
 
 @Controller('villagers')
 export class VillagersController {
   constructor(private readonly villagersService: VillagersService) {}
 
   @Get()
-  findAll() {
+  async findAll(): Promise<VillagerPreviewDTO[]> {
     return this.villagersService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.villagersService.findOne(+id);
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.villagersService.findOne(id);
   }
 }
